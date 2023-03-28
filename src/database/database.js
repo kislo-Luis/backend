2ab142d8eabb3527233e628be4bbbcd669028392
Kislo-Luis <luiskislo15@gmail.com>
2023-03-28 14:12:20 -0300

    feat:env.file

--- src/database/database.js
+++ src/database/database.js
@@ -1,7 +1,10 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
 import Sequelize from "sequelize";
-export const sequelize = new Sequelize("product", "luis", "asd123", {
-  hots: "localhot",
+export const sequelize = new Sequelize(process.env.DB_NAME,process.env.USER_DB, process.env.PASSWORD_DB, {
+  host: "localhost",
   dialect: "postgres",
 });
+
 //el orden aca es databaseName, userName, password
 //este export es para importarlo en index.js
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 import app from "./app.js";
 import { sequelize } from "./database/database.js";
 
@@ -5,8 +7,8 @@ import { sequelize } from "./database/database.js";
 async function main() {
   try {
     await sequelize.sync({ force: false });
-    app.listen(3000);
-    console.log("server run on port 3000");
+    app.listen(process.env.PORT);
+    console.log("server run on port " + process.env.PORT);
   } catch (error) {
     console.error("Unable to connect to the database:", error);
   }
